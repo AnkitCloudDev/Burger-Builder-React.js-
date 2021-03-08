9bb4e2df8e21af3cbe94d71d2c41ed797ff15e7b
unknown <ankitdongre96@gmail.com>
2021-03-08 22:42:33 +0000

    Added Ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -9,15 +9,14 @@ const burger = (props) => {
         return [...Array(props.ingredients[igKey])]
         .map(( _ , i) => {
            return <BurgerIngredient key ={igKey + i} type = {igKey}/>;
-        });
+        }).reduce((arr,curr) => { return arr.concat(el)}, [])
+        );
     } );
     return (
     <div className = {classes.Burger}>
-        <BurgerIngredient type = "bread-top" ></BurgerIngredient>
-        <BurgerIngredient type = "cheese" ></BurgerIngredient>
-        <BurgerIngredient type = "salad" ></BurgerIngredient>
-        <BurgerIngredient type = "bread-bottom" ></BurgerIngredient>
-        
+        <BurgerIngredient type = "burger-top" ></BurgerIngredient>
+        {burgerIngredients}
+        <BurgerIngredient type = "burger-bottom" ></BurgerIngredient>
 
     </div>
     );
