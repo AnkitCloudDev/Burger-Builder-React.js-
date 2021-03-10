5944636e56862985d4d59bab457234d7f82d1db2
unknown <ankitdongre96@gmail.com>
2021-03-10 21:04:38 +0000

    Added Basic Layout to Burger Controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Auxiliary from '../../components/Auxiliary/Auxiliary';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 class BurgerBuilder extends Component{
   
     state = {
@@ -15,6 +16,7 @@ class BurgerBuilder extends Component{
         return (
       <Auxiliary>        
           <Burger ingredients = {this.state.ingredients} ></Burger>
+          <BuildControls/>
       </Auxiliary>
         );
 
