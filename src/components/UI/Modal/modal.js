d8889774754880ee78602d08760f085e561d6fd4
unknown <ankitdongre96@gmail.com>
2021-03-28 12:12:22 +0100

    fixed import issues

--- src/components/UI/Modal/modal.js
+++ src/components/UI/Modal/modal.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import classes from './Modal.css';
 import Auxiliary from '../../Auxiliary/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
